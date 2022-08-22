40b8841a68fda170e53d5e77410a578fc3cb74b8
“Muhammad” <zabborovmuhammad291@gmail.com>
2022-08-22 22:32:13 +0500

    some changes

--- root/settings.py
+++ root/settings.py
@@ -52,11 +52,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
-    'django_filters'
+    'django_filters',
+    'corsheaders'
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
