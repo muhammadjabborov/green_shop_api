6e7adcb93bcd2a4fec95f9bcd676941d24db57d1
“Muhammad” <zabborovmuhammad291@gmail.com>
2022-09-15 21:58:06 +0500

    change product.py

--- apps/products/views/product.py
+++ apps/products/views/product.py
@@ -22,7 +22,7 @@ class ProductModelViewSet(ModelViewSet):
     queryset = Product.objects.order_by('id')
     serializer_class = ProductModelSerializer
     # permission_classes = (IsAuthenticated,)
-    # pagination_class = ProductPagination
+    pagination_class = ProductPagination
     parser_classes = (MultiPartParser,)
     search_fields = ['id', 'title', 'tag']
     lookup_url_kwarg = 'id'
@@ -55,6 +55,6 @@ class ProductImageModelViewSet(ModelViewSet):
     # permission_classes = (IsAuthenticated,)
     parser_classes = (MultiPartParser,)
     serializer_class = ProductImageModelSerializer
-    # pagination_class = ProductPagination
+    pagination_class = ProductPagination
     lookup_url_kwarg = 'id'
 
