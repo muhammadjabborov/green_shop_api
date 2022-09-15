d11242c076cf0472744c78ab4136214bb205d954
“Muhammad” <zabborovmuhammad291@gmail.com>
2022-09-15 22:06:55 +0500

    change product.py

--- apps/shared/rest_framework/pagination.py
+++ apps/shared/rest_framework/pagination.py
@@ -2,7 +2,9 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class ProductPagination(PageNumberPagination):
-    pass
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 10
 
 
 class CategoryPagination(PageNumberPagination):
